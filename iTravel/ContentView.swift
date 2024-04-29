1793e9513282fe909588103234d3e096ad8fda0b
ad-oliviero <adrianoliviero23@gmail.com>
2024-04-29 12:43:11 +0200

    Initial Commit

---
+++ iTravel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iTravel
+//
+//  Created by Adriano Oliviero on 29/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iTravel/iTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTravelApp.swift
+//  iTravel
+//
+//  Created by Adriano Oliviero on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
