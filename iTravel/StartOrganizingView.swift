18e3168f3557e3c9a6bf8be0ec057212827517bd
frnak2004 <franklinekoh@gmail.com>
2024-04-30 12:48:16 +0200

    qwertyuiop

---
+++ iTravel/StartOrganizingView.swift
@@ -0,0 +1,27 @@
+//
+//  StartOrganizingView.swift
+//  iTravel
+//
+//  Created by Franklyn Chiemeka Ekoh on 30/04/24.
+//
+
+import SwiftUI
+
+struct StartOrganizingView: View {
+    var body: some View {
+        Button(action:{
+            
+        }){
+            Text("Back")
+        }
+        Button(action:{
+            
+        }){
+            Text("Next")
+        }
+    }
+}
+
+#Preview {
+    StartOrganizingView()
+}
