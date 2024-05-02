8876fcf958e8dec9cf9fb000bdb35a93e650cb7c
ad-oliviero <adrianoliviero23@gmail.com>
2024-05-02 11:33:12 +0200

    Added autocomplete API

--- iTravel/APIWrappers/GeoAPIfy.swift
+++ iTravel/APIWrappers/GeoAPIfy.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 
+
 class GeoRequest {
-    private let apiKey = "2fd2e15f4fb94d7f90bd4f809e76c302"; // TODO: Make this shit secure
-    private var url: String = "https://"; // this will be updated in the functions below
-    private var response: Data? = nil;
-    private let httpUtils = CommonAPIUtils();
+    public var url: String = "https://api.geoapify.com/v1/" // this will be updated in the functions below
+    public var response: Data?, Decodable = Data()
+    private let apiKey = "2fd2e15f4fb94d7f90bd4f809e76c302" // TODO: Make this shit secure
+    private let httpUtils = CommonAPIUtils()
+    private let lang = "en" // TODO: Make this dynamic
     
-    public func sendRequest() async throws -> Void { response = try await httpUtils.responseRawData(url); }
+    public func sendRequest() async throws -> Void { response = try await httpUtils.httpsRequest(url: url + "&lang=\(lang)&apiKey=\(apiKey)") }
 }
 
 class IsolinesAPI: GeoRequest {
@@ -21,7 +23,6 @@ class IsolinesAPI: GeoRequest {
 }
 
 class PlacesAPI: GeoRequest {
-    
 }
 
 class PlacesDetailsAPI: GeoRequest {
--- iTravel/APIWrappers/TestAPIView.swift
+++ iTravel/APIWrappers/TestAPIView.swift
@@ -8,15 +8,16 @@
 import SwiftUI
 
 struct TestAPIView: View {
-    let utils = CommonAPIUtils()
     var body: some View {
         NavigationStack {
             VStack {
                 Button {
                     Task {
                         do {
-                            let response = try await utils.responseToDict("https://api.geoapify.com/v2/place-details?id=51068d4fb7a2fb2840594e25034015f24440f00103f901d535815700000000c00201920308436f6c6f7373656f&apiKey=2fd2e15f4fb94d7f90bd4f809e76c302")
-                            print(response)
+                            let request = AutoComplete(text: "Napo")
+                            try await request.sendRequest()
+                            //print(try await request.responseToString())
+                            print(try await request.responseToJson())
                         } catch {
                             print(error.localizedDescription)
                         }
