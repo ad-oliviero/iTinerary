62a362048a844a5b2996d08664fbd1963be957fc
ad-oliviero <adrianoliviero23@gmail.com>
2024-05-06 08:43:10 +0200

    Updated ProfileStepsView and PickInterestsView to use PlacesAPI structs

--- iTinerary/APIWrappers/PlacesAPI/CategoryEnum.swift
+++ iTinerary/APIWrappers/PlacesAPI/CategoryEnum.swift
@@ -5,12 +5,7 @@
 //  Created by Adriano Oliviero on 02/05/24.
 //
 
-import Foundation
-
-/// The rawvalue is a tuple in the following format: ("displayName", "apiRequest"). You only want to use the first one
-/// If the displayName is missing, it means that the category is not meant to be displayed
 enum Category: CaseIterable {
-  typealias RawValue = (String, String)
   case sport
   case adult
   case beach
@@ -20,32 +15,33 @@ enum Category: CaseIterable {
   case entertainment
   case man_made
   case natural
-  var rawValue: (String, String) {
+  var rawValue: DisplayableRequest {
     switch self {
     case .sport:
-      return ("Sport", "sport,ski,activity.sport_club,building.sport")
+      return DisplayableRequest("Sport", "sport,ski,activity.sport_club,building.sport")
     case .tourism:
-      return ("Tourism", "tourism,building.historic")
+      return DisplayableRequest("Tourism", "tourism,building.historic")
     case .adult:
-      return ("Adult", "adult")
+      return DisplayableRequest("Adult", "adult")
     case .beach:
-      return ("Beach", "beach")
+      return DisplayableRequest("Beach", "beach")
     case .camping:
-      return ("Camping", "camping")
+      return DisplayableRequest("Camping", "camping")
     case .catering:
-      return (
+      return DisplayableRequest(
         "Catering",
         "catering.bar,catering.biergarten,catering.cafe,catering.fast_food,catering.ice_cream,catering.pub,catering.restaurant"
       )
     case .entertainment:
-      return (
+      return DisplayableRequest(
         "Entertainment",
         "entertainment.activity_park.climbing,entertainment.activity_park.trampoline,entertainment.amusement_arcade,entertainment.aquarium,entertainment.cinema,entertainment.culture,entertainment.escape_game,entertainment.miniature_golf,entertainment.museum,entertainment.planetarium,entertainment.theme_park,entertainment.water_park,entertainment.zoo"
       )
     case .man_made:
-      return ("Man made", "man_made,man_made.bridge,man_made.pier,man_made.tower,man_made.windmill")
+      return DisplayableRequest(
+        "Man made", "man_made,man_made.bridge,man_made.pier,man_made.tower,man_made.windmill")
     case .natural:
-      return ("Natural", "natural,national_park")
+      return DisplayableRequest("Natural", "natural,national_park")
     }
   }
 }
--- iTinerary/PersonalInfoViews/PickInterestsView.swift
+++ iTinerary/PersonalInfoViews/PickInterestsView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PickInterestsView: View {
   @State var selectedCategories: [String: Bool] = Category.allCases.reduce(into: [:]) {
     result, category in
-    result[category.rawValue.0] = false
+    result[category.rawValue.displayName] = false
   }
 
   var body: some View {
@@ -21,16 +21,17 @@ struct PickInterestsView: View {
           .padding()
 
         ScrollView {
-          LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 16) {
-            ForEach(Category.allCases, id: \.self) { category in
+          LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 32) {
+            ForEach(Array(selectedCategories.enumerated()), id: \.element.key) { index, pair in
+              let category = pair.key
+              let selected = pair.value
               Button(action: {
-                selectedCategories[category.rawValue.0]?.toggle()
+                selectedCategories[category]?.toggle()
               }) {
-                let isSelected = selectedCategories[category.rawValue.0]!
-                Text(category.rawValue.0)
+                Text(category)
                   .padding()
-                  .background(isSelected ? Color.blue : Color.blue.opacity(0.2))
-                  .foregroundColor(isSelected ? .white : .blue)
+                  .background(selected ? Color.blue : Color.blue.opacity(0.2))
+                  .foregroundColor(selected ? .white : .blue)
                   .cornerRadius(10)
               }
               .buttonStyle(PlainButtonStyle())
@@ -50,8 +51,7 @@ struct PickInterestsView: View {
                 .font(.system(size: 15))
                 .font(.title)
             }
-          }
-          .disabled(selectedCategories.filter { $0.1 }.count < 3)
+          }.disabled(selectedCategories.filter { $0.1 }.count < 3)
       )
     }
   }
