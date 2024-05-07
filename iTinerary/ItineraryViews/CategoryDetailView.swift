a180b3b85a951d8fd04a49f1ba617f0d86975a66
danielalandolfo0 <daniela.landolfo03@gmail.com>
2024-05-07 10:39:54 +0200

    tried to have the places request

--- iTinerary/ItineraryViews/CategoryDetailView.swift
+++ iTinerary/ItineraryViews/CategoryDetailView.swift
@@ -1,30 +1,66 @@
+// CategoryDetailView.swift
 import SwiftUI
 
 struct CategoryDetailView: View {
     var selectedCategories: [Category: Bool]
     let categoryName: String
     let index: Int
+    @State private var placeId: String = ""
+    let myCity = SharedCity()
+    
+    init(selectedCategories: [Category: Bool], categoryName: String, index: Int) {
+        self.selectedCategories = selectedCategories
+        self.categoryName = categoryName
+        self.index = index
+    }
     
     var body: some View {
         NavigationStack {
             VStack {
-              //let request = PlacesAPIRequest(placeId: "yourPlaceId", categories: selectedCategories.keys, conditions: [:], limit: 10, offset: 0)
+                // Visualizza il nome della categoria in maiuscolo
+                Text(categoryName.capitalized)
+                   .navigationTitle(categoryName.capitalized)
+                Spacer()
             }
-            .navigationTitle(" \(categoryName.capitalized)")
-            .navigationBarItems(
+           .navigationBarItems(
                 trailing: NavigationLink(destination: NextCategoryDetailView(selectedCategories: selectedCategories, currentIndex: index)) {
                     HStack {
                         Text("Next")
                         Image(systemName: "chevron.right")
-                            .font(.system(size: 15))
-                            .font(.title)
+                           .font(.system(size: 15))
+                           .font(.title)
                     }
                 }
             )
+           .onAppear {
+                self.fetchPlaceId(for: myCity.creating[0].name)
+            }
         }
     }
+    
+  private func fetchPlaceId(for city: String) {
+      Task {
+          do {
+              let request = AutoCompleteAPIRequest(text: city)
+              try await request.sendRequest()
+
+              if let firstFeature = try? await request.responseToJson().features?.first {
+                  DispatchQueue.main.async {
+                      self.placeId = firstFeature.properties?.id ?? ""
+                  }
+              }
+
+              let request1 = PlacesAPIRequest(placeId: self.placeId, categories: Array(selectedCategories.filter { $0.value }.keys), conditions: [:], limit: 10, offset: 0)
+            ///print(request1.response)
+          } catch {
+              print("Error fetching place ID: \(error)")
+          }
+      }
+  }
+  
 }
 
+// NextCategoryDetailView.swift
 struct NextCategoryDetailView: View {
     var selectedCategories: [Category: Bool]
     let currentIndex: Int
@@ -34,7 +70,7 @@ struct NextCategoryDetailView: View {
         let nextIndex = currentIndex + 1
         if nextIndex < categoryKeys.count {
             let nextCategory = categoryKeys[nextIndex]
-          let nextCategoryName = nextCategory.rawValue.displayName
+            let nextCategoryName = nextCategory.rawValue.displayName
             return AnyView(CategoryDetailView(selectedCategories: selectedCategories, categoryName: nextCategoryName, index: nextIndex))
         } else {
             // Handle if there's no next category
@@ -50,4 +86,3 @@ struct CategoryDetailView_Previews: PreviewProvider {
     }
 }
 #endif
-
